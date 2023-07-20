f1d8f346dfa5932a894b90f60198ff06bb6ffff8
diego <smartcrashomg@gmail.com>
2023-07-20 23:12:00 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -37,59 +37,62 @@ const experiece: {
   bulletPoints: string[];
   technologies: string[];
 }[] = [
-  {
-    company: "Cliengo",
-    position: "Back-End Developer",
-    startDate: new Date(2021, 8, 0),
-    endDate: new Date(2022, 3, 0),
-    bulletPoints: [
-      `Developed custom features for a Chatbot and helped several clients who had different needs for their business to bring a better customer service with their Chatbot`,
-    ],
-    technologies: ["Node.js", "AWS Lambdas", "API Gateway"],
-  },
+    {
+      company: "Cliengo",
+      position: "Back-End Developer",
+      startDate: new Date(2021, 8, 0),
+      endDate: new Date(2022, 3, 0),
+      bulletPoints: [
+        "Spearheaded the development of custom features for an advanced Chatbot, tailoring solutions to diverse client requirements",
+        "Leveraged expertise in <strong>Node.js</strong> and <strong>AWS Lambdas</strong> to optimize Chatbot performance, resulting in a significant enhancement in overall user experience and client satisfaction."
+      ],
+      technologies: ["Node.js", "AWS Lambdas", "API Gateway"],
+    },
 
-  {
-    company: "Ordde S.A",
-    position: "Full-stack Web Developer",
-    startDate: new Date(2021, 1, 0),
-    endDate: new Date(2021, 8, 0),
-    bulletPoints: [
-      "Contributed to the development of a large application. Here I had the opportunity to work with a team and learned how they work together. I quickly adapted to their workflow and their tech stack which allow me to start adding new features in a couple of weeks.",
-    ],
-    technologies: ["JavaScript", "Meteor JS", "MongoDB", "CircleCI"],
-  },
+    {
+      company: "Ordde S.A",
+      position: "Full-stack Web Developer",
+      startDate: new Date(2021, 1, 0),
+      endDate: new Date(2021, 8, 0),
+      bulletPoints: [
+        "Played a pivotal role in the development of a substantial application, collaborating closely with a talented team, and gaining valuable insights into effective teamwork dynamics.",
+        "Demonstrated a remarkable ability to swiftly assimilate the team's workflow and proficiently navigate their tech stack, including <strong>JavaScript</strong>, <strong>Meteor JS</strong>, <strong>MongoDB</strong>, enabling me to contribute to new feature implementation within a mere couple of weeks.",
+      ],
+      technologies: ["JavaScript", "Meteor JS", "MongoDB", "CircleCI"],
+    },
 
-  {
-    company: "Workana",
-    position: "Freelancer",
-    startDate: new Date(2020, 3, 0),
-    endDate: new Date(2021, 1, 0),
-    bulletPoints: [
-      "Completed successfully several projects receiving excellent qualification from my clients.",
-      "All my projects are related with JavaScript and\or PHP",
-      "Working as freelancer have improved my communication and adaptability skills, valuable skills in this industry.",
-    ],
-    technologies: [
-      "JavaScript",
-      "Vue",
-      "PHP",
-      "Laravel",
-      "MySQL",
-      "PostgreSQL",
-    ],
-  },
+    {
+      company: "Workana",
+      position: "Freelancer",
+      startDate: new Date(2020, 3, 0),
+      endDate: new Date(2021, 1, 0),
+      bulletPoints: [
+        "Consistently achieved outstanding client satisfaction by successfully delivering a diverse range of projects, all centered around <strong>JavaScript</strong> and/or <strong>PHP</strong> technologies.",
+        "As a proactive freelancer, honed exceptional communication and adaptability skills, proving instrumental in navigating the demands of this dynamic industry.",
+        "Proficiently utilized a wide array of technologies, including <strong>JavaScript</strong>, <strong>Vue</strong>, <strong>PHP</strong>, <strong>Laravel</strong>, and databases like <strong>MySQL</strong> and <strong>PostgreSQL</strong>, to develop innovative solutions that exceed client expectations."
+      ],
+      technologies: [
+        "JavaScript",
+        "Vue",
+        "PHP",
+        "Laravel",
+        "MySQL",
+        "PostgreSQL",
+      ],
+    },
 
-  {
-    company: "System Click s.a",
-    position: "Junior Software Developer ",
-    startDate: new Date(2017, 10, 0),
-    endDate: new Date(2019, 12, 0),
-    bulletPoints: [
-      "Collaborated in the development of new products such as Administrative Software, Desktop Applications, CMS and Web Sites, acquiring solid experience on the field.",
-    ],
-    technologies: ["JavaScript", "Vue", "Electron", "MongoDB", "PostgreSQL"],
-  },
-];
+    {
+      company: "System Click s.a",
+      position: "Junior Software Developer ",
+      startDate: new Date(2017, 10, 0),
+      endDate: new Date(2019, 12, 0),
+      bulletPoints: [
+        "Collaborated on the successful development of diverse products, ranging from Administrative Software and Desktop Applications to CMS and Web Sites, contributing to the acquisition of a robust skill set in the field.",
+        "Proficiently employed a variety of technologies, including <strong>JavaScript</strong>, <strong>Vue</strong>, <strong>Electron</strong> for desktop applications, and database management systems like <strong>MongoDB</strong> and <strong>PostgreSQL</strong>, to deliver high-quality solutions that met client requirements and industry standards."
+      ],
+      technologies: ["JavaScript", "Vue", "Electron", "MongoDB", "PostgreSQL"],
+    },
+  ];
 
 const certifications: {
   title: string;
@@ -97,42 +100,42 @@ const certifications: {
   url: string;
   date: Date;
 }[] = [
-  {
-    title: "Machine Learning",
-    emitter: "Coursera",
-    date: new Date(2021, 8, 0),
-    url: "https://www.coursera.org/account/accomplishments/verify/YNUELBFRPXER",
-  },
+    {
+      title: "Machine Learning",
+      emitter: "Coursera",
+      date: new Date(2021, 8, 0),
+      url: "https://www.coursera.org/account/accomplishments/verify/YNUELBFRPXER",
+    },
 
-  {
-    title: "Natural Language Processing with Classification and Vector Spaces",
-    emitter: "Coursera",
-    date: new Date(2021, 10, 0),
-    url: "https://www.coursera.org/account/accomplishments/certificate/452UQT3FUFGM",
-  },
+    {
+      title: "Natural Language Processing with Classification and Vector Spaces",
+      emitter: "Coursera",
+      date: new Date(2021, 10, 0),
+      url: "https://www.coursera.org/account/accomplishments/certificate/452UQT3FUFGM",
+    },
 
-  {
-    title:
-      "Divide and Conquer, Sorting and Searching, and Randomized Algorithms",
-    emitter: "Coursera",
-    date: new Date(2021, 11, 0),
-    url: "https://www.coursera.org/account/accomplishments/verify/9C9T4CXA4DXK",
-  },
+    {
+      title:
+        "Divide and Conquer, Sorting and Searching, and Randomized Algorithms",
+      emitter: "Coursera",
+      date: new Date(2021, 11, 0),
+      url: "https://www.coursera.org/account/accomplishments/verify/9C9T4CXA4DXK",
+    },
 
-  {
-    title: "APIs and Microservices",
-    emitter: "freeCodeCamp",
-    date: new Date(2021, 7, 0),
-    url: "https://freecodecamp.org/certification/diegodasilva/apis-and-microservices",
-  },
+    {
+      title: "APIs and Microservices",
+      emitter: "freeCodeCamp",
+      date: new Date(2021, 7, 0),
+      url: "https://freecodecamp.org/certification/diegodasilva/apis-and-microservices",
+    },
 
-  {
-    title: "JavaScript Algorithms and Data Structures",
-    emitter: "freeCodeCamp",
-    date: new Date(2022, 5, 0),
-    url: "https://freecodecamp.org/certification/diegodasilva/javascript-algorithms-and-data-structures",
-  },
-];
+    {
+      title: "JavaScript Algorithms and Data Structures",
+      emitter: "freeCodeCamp",
+      date: new Date(2022, 5, 0),
+      url: "https://freecodecamp.org/certification/diegodasilva/javascript-algorithms-and-data-structures",
+    },
+  ];
 
 const projects: {
   name: string;
@@ -141,69 +144,61 @@ const projects: {
   github: string;
   technologies: string[];
 }[] = [
-  {
-    name: "Liboards",
-    excerpt: "An Issue Tracker to organize your projects",
-    website: "https://liboards.space",
-    github: "https://github.com/smartcrash/liboards",
-    technologies: [
-      "React",
-      "Express",
-      "GraphQL",
-      "TypeScript",
-      "Heroku",
-      "Vercel",
-      "Vitest",
-      "Jest",
-      "Cypress",
-    ],
-  },
+    {
+      name: "Liboards",
+      excerpt: "An Issue Tracker to organize your projects",
+      website: "https://liboards.space",
+      github: "https://github.com/smartcrash/liboards",
+      technologies: [
+        "React",
+        "Express",
+        "GraphQL",
+        "TypeScript",
+        "Heroku",
+        "Vercel",
+        "Vitest",
+        "Jest",
+        "Cypress",
+      ],
+    },
 
-  {
-    name: "Another TODO List",
-    excerpt: "A simple and beautiful TODO list app",
-    github: "https://github.com/smartcrash/another-todo-list",
-    technologies: [
-      "React",
-      "Express",
-      "GraphQL",
-      "TypeScript",
-      "Vitest",
-      "Jest",
-      "Cypress",
-    ],
-  },
-];
+    {
+      name: "Another TODO List",
+      excerpt: "A simple and beautiful TODO list app",
+      github: "https://github.com/smartcrash/another-todo-list",
+      technologies: [
+        "React",
+        "Express",
+        "GraphQL",
+        "TypeScript",
+        "Vitest",
+        "Jest",
+        "Cypress",
+      ],
+    },
+  ];
 </script>
 
 <template>
-  <main
-    class="mx-auto text-gray-700 space-y-5 px-10 pt-5"
-    :style="{ maxWidth: '1200px' }"
-  >
+  <main class="mx-auto text-gray-700 space-y-5 px-10 pt-5" :style="{ maxWidth: '1200px' }">
     <section class="text-center relative">
       <h1 class="text-3xl font-mono font-black tracking-tighter">
         Diego Da'Silva
       </h1>
       <h5 class="space-x-3 mt-2">
-        <span
-          ><a href="mailto:smartcrashomg@gmail.com" class="underline">
+        <span><a href="mailto:smartcrashomg@gmail.com" class="underline">
             <EnvelopeIcon />
-            smartcrashomg@gmail.com</a
-          >
+            smartcrashomg@gmail.com
+          </a>
         </span>
         <span>|</span>
         <a href="tel:+310622575275">+31 06 2257 52 75</a>
         <span>|</span>
         <span>
-          <a
-            href="https://linkedin.com/in/diego-da-silva-developer"
-            class="underline"
-            target="_blank"
-          >
+          <a href="https://linkedin.com/in/diego-da-silva-developer" class="underline" target="_blank">
             <LinkedinIcon class="pr-1" />
-            linkedin.com/in/diego-da-silva-developer</a
-          >
+            linkedin.com/in/diego-da-silva-developer
+          </a>
         </span>
       </h5>
     </section>
@@ -215,12 +210,12 @@ const projects: {
 
       <dl class="grid grid-cols-4 pl-4 gap-1">
         <template v-for="(chunks, index) in chunk(skills, 4)">
-            <ul class="list-disc">
-              <li v-for="(skill, index) in chunks">
-                {{ skill }}
-              </li>
-            </ul>
-          </template>
+          <ul class="list-disc">
+            <li v-for="(skill, index) in chunks">
+              {{ skill }}
+            </li>
+          </ul>
+        </template>
       </dl>
     </section>
 
@@ -230,54 +225,45 @@ const projects: {
       <Divider />
 
       <ul class="space-y-3 overflow-hidden">
-        <li
-          v-for="{
-            company,
-            position,
-            startDate,
-            endDate,
-            bulletPoints,
-            technologies,
-          } in experiece"
-        >
+        <li v-for="{
+          company,
+          position,
+          startDate,
+          endDate,
+          bulletPoints,
+          technologies,
+        } in experiece">
           <article class="relative pl-10">
             <span class="flex col-span-2 items-center absolute top-1 left-0">
               <svg viewBox="0 0 12 12" class="w-3 h-3 mr-6 overflow-visible">
                 <circle cx="6" cy="6" r="6" fill="currentColor"></circle>
-                <path
-                  d="M 6 18 V 500"
-                  fill="none"
-                  stroke-width="2"
-                  stroke="currentColor"
-                ></path>
+                <path d="M 6 18 V 500" fill="none" stroke-width="2" stroke="currentColor"></path>
               </svg>
             </span>
 
             <div class="space-y-2">
-              <p class="text-sm">
-                {{ formatDate(startDate) }} - {{ formatDate(endDate) }}
-              </p>
-
-              <div class="flex space-x-2 text-xl">
-                <h5 class="font-bold">{{ company }}</h5>
-                <span>|</span>
-                <h4 class="">{{ position }}</h4>
+              <div>
+                <h5 class="font-bold text-lg">{{ company }}</h5>
+                <div class="flex items-center space-x-2">
+                  <h4 class="italic">{{ position }}</h4>
+                  <span>|</span>
+                  <p class="text-sm">
+                    {{ formatDate(startDate) }} - {{ formatDate(endDate) }}
+                  </p>
+                </div>
               </div>
 
-              <ul class="list-disc">
-                <li v-for="point in bulletPoints">
-                  {{ point }}
+              <ul class="list-disc pl-3 text-sm">
+                <li v-for="point in bulletPoints" v-html="point">
                 </li>
               </ul>
 
-              <div>
+              <div class="hidden">
                 <p class="font-bold">Technologies:</p>
                 <ul class="flex">
                   <li v-for="(tech, index) in technologies">
                     {{ tech }}
-                    <span v-if="index < technologies.length - 1" class="mx-2"
-                      >•</span
-                    >
+                    <span v-if="index < technologies.length - 1" class="mx-2">•</span>
                   </li>
                 </ul>
               </div>
@@ -287,14 +273,12 @@ const projects: {
       </ul>
     </section>
 
-    <section>
+    <section class="hidden">
       <Heading>Projects</Heading>
       <Divider />
 
       <ul class="grid grid-cols-2 grid-flow-col list-disc gap-16">
-        <li
-          v-for="{ name, excerpt, website, github, technologies } in projects"
-        >
+        <li v-for="{ name, excerpt, website, github, technologies } in projects">
           <div class="flex space-x-2">
             <h3 class="text-lg font-bold">{{ name }}</h3>
 
@@ -302,25 +286,22 @@ const projects: {
               <span>|</span>
               <a :href="website" class="underline" target="_blank">
                 <LinkIcon class="pr-1" />
-                {{ website.replace("https://", "") }}</a
-              >
+                {{ website.replace("https://", "") }}
+              </a>
             </template>
 
             <span>|</span>
             <a :href="github" class="underline" target="_blank">
               <GitHubIcon class="pr-1" />{{
                 github?.replace("https://", "")
-              }}</a
-            >
+              }}
+            </a>
           </div>
           <p class="text-sm">{{ excerpt }}</p>
 
           <p class="font-bold mt-2 mb-1">Technologies:</p>
           <ul class="flex flex-wrap space-x-1 text-sm">
-            <li
-              v-for="(technologie, index) in technologies"
-              class="flex items-center"
-            >
+            <li v-for="(technologie, index) in technologies" class="flex items-center">
               {{ technologie }}
               <span v-if="index < technologies.length - 1" class="mx-2">•</span>
             </li>
@@ -346,12 +327,8 @@ const projects: {
         </li>
       </ul>
 
-      <a
-        class="underline block pt-5"
-        href="https://www.linkedin.com/in/diego-da-silva-8a06411a2/details/certifications/"
-        target="_blank"
-        >See all certifications ></a
-      >
+      <a class="underline block pt-5" href="https://www.linkedin.com/in/diego-da-silva-8a06411a2/details/certifications/"
+        target="_blank">See all certifications ></a>
     </section>
   </main>
 </template>
